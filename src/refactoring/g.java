64e3bd58cd113d1441106fc929b134fdf41df8f8
gauri <gauriharmalkar98@gmail.com>
2017-06-07 16:16:41 +0530

    hi

---
+++ src/refactoring/g.java
@@ -0,0 +1,21 @@
+package refactoring;
+
+public class g {
+	public static void main(String[] args, int newParam){
+		int[] marks123={1,2,3};
+		int markss=10;
+		marki12t(marks123);
+		System.out.println(markss*10);
+		System.out.println(markss*10);
+		
+	}
+
+	public static void marki12t(int[] marks) {
+		for(int mark:marks)
+		{
+			System.out.println(mark);
+		}
+	}
+
+
+}
